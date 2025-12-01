b9f0e1aad8f260d76c3383325482526b875d7456
khalid <khalid.ibr212@gmail.com>
2025-12-01 20:27:51 +0300

    chore: Update Prisma configuration paths in prisma.config.ts and adjust build script in package.json for improved clarity and organization.

--- server/prisma/prisma.config.ts
+++ server/prisma/prisma.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "prisma/config";
 // import { env } from "process";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "schema.prisma",
   migrations: {
     // seed: "tsx prisma/seed.ts",
-    path: "prisma/migrations",
+    path: "migrations",
   },
   datasource: {
     url: process.env.DATABASE_URL || "postgresql://postgres:password@localhost:5432/chat-app",
